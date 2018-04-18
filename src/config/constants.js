b50e787b477860577e784d877695e5771f4bc48c
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2018-04-18 13:46:17 -0500

    add files

---
+++ src/config/constants.js
@@ -0,0 +1,11 @@
+import Rebase from 're-base';
+import firebase from 'firebase';
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyBr9aWKUnBaPNDmFfdfVcETevq3GCEicF8",
+    authDomain: "react-music-history.firebaseapp.com",
+    databaseURL: "https://react-music-history.firebaseio.com"
+});
+export const rebase = Rebase.createClass(app.database());
+
+// //add the authProvides your app needs: google, facebook, twitter, github,
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
